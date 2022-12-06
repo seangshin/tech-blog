57cc3c8f041c24b71be2b13e1dab3633fe35b492
Sean Shin <shin.seang@gmail.com>
2022-12-06 12:09:08 -0500

    controller and server.js updated with basic functionality

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 //Dependencies
 const express = require('express');
 const path = require('path');
+const routes = require('./controllers');
+
+//
+const sequelize = require('./config/connection');
 
 //Set up for express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up for the routes
-app.use(require('./controllers'));
+app.use(routes);
 
 // Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log(`Sever listening on http://localhost:${PORT}`);
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log(`Sever listening on http://localhost:${PORT}`));
 });
