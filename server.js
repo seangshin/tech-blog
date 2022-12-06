c42cd7268592bd5299522e354b4161d36a804a27
Sean Shin <shin.seang@gmail.com>
2022-12-05 20:26:20 -0500

    added setup boilerplates

---
+++ server.js
@@ -0,0 +1,17 @@
+//Dependencies
+const express = require('express');
+const path = require('path');
+
+//Set up for express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up for the routes
+app.use(require('./controllers'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`Sever listening on http://localhost:${PORT}`);
+});
