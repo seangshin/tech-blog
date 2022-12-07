2292f1c226dad92d90a61b675fd89588dc8062c4
seangshin <110693087+seangshin@users.noreply.github.com>
2022-12-07 16:16:25 -0500

    Revert "Feature/views"

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -25,8 +25,21 @@ router.get('/', async (req, res) => {
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
-    const commentData = await Comment.findAll({
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    // get current comments
+    const currentComments = await Comment.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -35,11 +48,38 @@ router.post('/', withAuth, async (req, res) => {
       ],
     });
 
-    //Serialize data so the template can read it
-    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    const comments = currentComments.get({ plain: true });
 
+    //serialize data
     console.log(comments);
-    res.status(200);
+    res.status(200).json(comments);
+
+    // const postData = await Post.update({
+    //   ...req.body,
+    //   user_id: req.session.user_id,
+    // });
+
+    // res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
